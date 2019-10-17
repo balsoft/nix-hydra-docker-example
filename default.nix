13339937c1e8422a9f326e00274298e94d211bb0
Alexander Bantyev <balsoft@yandex.ru>
2019-10-17 14:42:11 +0300

    Initial commit

---
+++ default.nix
@@ -0,0 +1,11 @@
+let sources = import ./nix/sources.nix;
+in { pkgs ? import sources.nixpkgs nixpkgsArgs
+, supportedSystems ? [ "x86-64_linux" ]
+, nixpkgsArgs ? { config.inHydra = true; }
+, releaseLib ?
+  import "${sources.nixpkgs}/pkgs/top-level/release-lib.nix" {
+    inherit nixpkgsArgs supportedSystems;
+  } }:
+pkgs.lib.genAttrs supportedSystems
+(system: { mc = (releaseLib.pkgsFor system).mc; })
+
