fdc3585c0730be9b91dc709a261efcfec698a72e
Alexander Bantyev <balsoft@yandex.ru>
2019-10-17 15:01:15 +0300

    Add docker

--- default.nix
+++ default.nix
@@ -8,5 +8,12 @@ in
     inherit nixpkgsArgs supportedSystems;
   } }:
 pkgs.lib.genAttrs supportedSystems
-(system: { mc = (releaseLib.pkgsFor system).mc; })
+(system: { 
+  mc = (releaseLib.pkgsFor system).mc; 
+  docker.mc = pkgs.dockerTools {
+    name = "mc";
+    contents = with pkgs; [stdenv coreutils bash mc];
+    config = { Cmd = [ "/bin/bash" ]; };
+  };
+})
 
